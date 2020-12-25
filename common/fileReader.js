2d6cad3cb9cbdd1e0870ce8efdefa8dc3a57727b
ruixin <IT@TH-RUIXCH-03.fareast.corp.microsoft.com>
2020-12-25 11:35:22 +0800

    add data

---
+++ common/fileReader.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+const readFileContent = (filePath) => {
+    return new Promise((resolve, reject)=>{
+        fs.readFile(filePath, 'utf8' , (err, data) => {
+            if (err) {
+              reject(err);
+            }
+            else{
+                resolve(data);
+            }
+          })
+    })
+}
+
+const convertTextToArray = (rawText) => {
+    let jsString = "const codes = [";
+
+    const lines = rawText.split("\n");
+    for(let i = 0; i < lines.length; i++ ){
+        let tempStr = `"${lines[i].trim()}"`;
+        if(i != lines.length -1) tempStr += ",";
+        else tempStr += "];\n";
+
+        jsString += tempStr;
+    }
+
+    jsString += "module.exports = codes;";
+
+    return jsString;
+}
+
+
+const writeContentToFile = (filePath, content) => {
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(filePath, content, err => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve('success');
+            }
+        })
+    })
+}
+
+readFileContent('../data/case.10000.16.txt')
+    .then( data => {
+        const toBeWritten = convertTextToArray(data);
+        // console.log(toBeWritten);
+        return writeContentToFile('../data/test.js', toBeWritten);
+    })
+    .then( (data)=>{
+        console.log(data);
+        const js = require("../data/test");
+        console.log(js[0]);
+    })
+    .catch( err => console.log(err));
