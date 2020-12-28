79f33740ac7b1242e9073a22bdef2fc2214fd755
ruixin <IT@TH-RUIXCH-03.fareast.corp.microsoft.com>
2020-12-28 17:00:43 +0800

    ready for words random generation

--- searchingAlgo/test2.js
+++ searchingAlgo/test2.js
@@ -1,10 +1,29 @@
-const haystacks = require("./../data/test");
+const haystacks = require("./../data/test2");
+// const haystacks = [
+//     "I went to hospital yesterday",
+//     "Today is a good day to die",
+//     "I am about to the town, would you come?",
+//     "Am I talking like that?",
+//     "I went to the old town about three years ago?",
+//     "Thinking helps clean your head",
+//     "We actually are doing a similarity test",
+//     "See how algo of this could find similar items",
+//     "So now, I am gonna create small relatively similar items",
+//     "To see how good this algo could be",
+//     "This is a sentence ended with a dog",
+//     "This is a sentence ended with a cat",
+//     "This is not a similar sentence comparing to former two",
+//     "A cat is eating a dog at the end of this sentence",
+//     "Dog went to hospital by the cat",
+//     "Done",
+//     "None"
+// ]
 const {findSimilarItems, findReapeated} = require("./MinHashLsh");
-const {getSimilarity} = require("./FuzzyMatcher");
+// const {getSimilarity} = require("./FuzzyMatcher");
 
-const shingleLength = 3;
-const bands = 10;
-const rows = 3;
+const shingleLength = 5;
+const bands = 8;
+const rows = 2;
 console.time("a");
 const buckets = findSimilarItems(haystacks, shingleLength, bands , rows);
 
