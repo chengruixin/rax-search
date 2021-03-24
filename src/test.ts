2a0ad84677ac3a0d4eccedffd193a67ec045bcd7
chengruixin <chengruixin_2010@163.com>
2021-03-25 06:37:44 +0800

    [code] replace distance calculator to .ts file

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,25 @@
 import {
-    getRandomArray
+    getShinglesDisregardRepeated,
+    normalizeToVectors
 } from './common/Computer';
 
+import {
+    getLvstnDistance,
+    getCosDistance
+} from './common/DistanceCalculator';
+
 function run() : void {
-    const arr = getRandomArray(10);
-    const arr2 = getRandomArray(10);
-    console.log(arr,arr2);
+    const string1 = "abcbASDASc";
+    const string2 = "fdloAASDFAokup";
+    
+    const shingles1 = getShinglesDisregardRepeated(string1, 2);
+    const shingles2 = getShinglesDisregardRepeated(string2, 2);
+
+    // console.log(shingles1, shingles2)
+    const result = normalizeToVectors([shingles1, shingles2]);
+    // console.log(result);
+    console.log(getLvstnDistance(string1, string2));
+    console.log(getCosDistance(shingles1, shingles2));
 }
 
 export default run;
