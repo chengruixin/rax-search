aead661dbb98df1abb67325e62953d3786de986b
chengruixin <chengruixin_2010@163.com>
2021-03-25 18:29:12 +0800

    [code] complete hash string fn

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 import {
     getShinglesDisregardRepeated,
-    normalizeToVectors
+    normalizeToVectors,
+    hashString
 } from './common/Computer';
 
 import {
@@ -8,18 +9,29 @@ import {
     getCosDistance
 } from './common/DistanceCalculator';
 
-function run() : void {
-    const string1 = "abcbASDASc";
-    const string2 = "fdloAASDFAokup";
-    
-    const shingles1 = getShinglesDisregardRepeated(string1, 2);
-    const shingles2 = getShinglesDisregardRepeated(string2, 2);
 
-    // console.log(shingles1, shingles2)
-    const result = normalizeToVectors([shingles1, shingles2]);
-    // console.log(result);
-    console.log(getLvstnDistance(string1, string2));
-    console.log(getCosDistance(shingles1, shingles2));
+import {
+    produceSimilarItems
+} from './FuzzyMatch/FuzzyMatcher';
+function run() : void {
+    const string1 = "w";
+    const string2 = "drink water";
+    const string3 = "watermelon"
+
+    console.log(hashString(string1));
+    console.log(hashString(string2));
+    console.log(hashString(string3));
+    
+    // const shingles1 = getShinglesDisregardRepeated(string1, 2);
+    // const shingles2 = getShinglesDisregardRepeated(string2, 2);
+
+    // // console.log(shingles1, shingles2)
+    // const result = normalizeToVectors([shingles1, shingles2]);
+    // // console.log(result);
+    // console.log(getLvstnDistance(string1, string2));
+    // console.log(getCosDistance(shingles1, shingles2));
+
+    // produceSimilarItems(["df", "fd"], "f", )
 }
 
 export default run;
